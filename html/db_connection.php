79f37d51eebc43846a817a40cd71e02094f15366
jluxeg <141065495+jluxeg@users.noreply.github.com>
2024-02-08 13:24:37 -0500

    Update db_connection.php

--- html/db_connection.php
+++ html/db_connection.php
@@ -1,6 +1,6 @@
 <?php
 function OpenCon(){
-	$dbhost = "realview-db-1";
+	$dbhost = "realview-db-1"; //if pulling directly from github  this could be "realview-master-db-1", double check with docker ps on the correct name to use
 	$dbuser = "root";
 	$dbpass = "root";
 	$dbname = "realviewdb";
